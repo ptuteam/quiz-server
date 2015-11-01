9f5862d960b88276a10b0f9b3e5de88619d170a0
alex <sashaudalv@gmail.com>
2015-11-02 01:29:18 +0300

    GuestSignInServletTest fix

--- src/test/java/servlets/GuestSignInServletTest.java
+++ src/test/java/servlets/GuestSignInServletTest.java
@@ -1,17 +1,19 @@
 package servlets;
 
+import org.junit.Before;
 import org.junit.Test;
 import utils.AccountService;
 import utils.AccountServiceImpl;
-import utils.SessionHelper;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -19,20 +21,30 @@ import static org.mockito.Mockito.when;
  * Created by Dima on 26.10.2015.
  */
 public class GuestSignInServletTest {
-    private final static HttpServletRequest request = mock(HttpServletRequest.class);
-    private final static HttpServletResponse response = mock(HttpServletResponse.class);
-    private final static AccountService accountService = new AccountServiceImpl();
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+    private AccountService accountService;
+    private HttpSession session;
+
+    @Before
+    public void setUp() {
+        request = mock(HttpServletRequest.class);
+        response = mock(HttpServletResponse.class);
+        accountService = new AccountServiceImpl();
+        session = mock(HttpSession.class);
+    }
 
     @Test
-    public void testGuestSignIn() throws Exception {
-        final String sessionId = "session";
-        StringWriter stringWriter = new StringWriter();
+    public void testGuestSignIn() throws ServletException, IOException {
+        try (StringWriter stringWriter = new StringWriter()) {
+            when(session.getId()).thenReturn("session");
+            when(request.getSession()).thenReturn(session);
+            when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
 
-        when(request.getSession()).thenReturn(SessionHelper.getNewSession(sessionId));
-        when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
+            new GuestSignInServlet(accountService).doGet(request, response);
 
-        new GuestSignInServlet(accountService).doPost(request, response);
-
-        assertTrue(accountService.isLogged(sessionId));
+            assertTrue(accountService.isLogged("session"));
+            assertTrue(accountService.getUserBySession("session").isGuest());
+        }
     }
 }
