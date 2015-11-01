e8125ca9c582f98f07743a64d2b4c552b9f02d38
alex <sashaudalv@gmail.com>
2015-11-02 01:24:40 +0300

    AdministrationServletTest fix

--- src/test/java/servlets/AdministrationServletTest.java
+++ src/test/java/servlets/AdministrationServletTest.java
@@ -1,89 +1,100 @@
 package servlets;
 
 import model.UserProfile;
-import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 import utils.AccountService;
 import utils.AccountServiceImpl;
-import utils.SessionHelper;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.*;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by dima on 01.11.15.
  */
 public class AdministrationServletTest {
-    private final static HttpServletRequest request = mock(HttpServletRequest.class);
-    private final static HttpServletResponse response = mock(HttpServletResponse.class);
-    private final static AccountService accountService = new AccountServiceImpl();
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+    private AccountService accountService;
+    private HttpSession session;
 
-    @Test
-    public void testNotLoggedIn() throws Exception {
-        StringWriter stringWriter = new StringWriter();
+    @Rule
+    private final ExpectedSystemExit exit = ExpectedSystemExit.none();
 
+    @Before
+    public void setUp() {
+        request = mock(HttpServletRequest.class);
+        response = mock(HttpServletResponse.class);
+        session = mock(HttpSession.class);
+        accountService = new AccountServiceImpl();
+        accountService.signUp(new UserProfile("admin", "admin", "sashaudalv@gmail.com", "avatar"));
         accountService.signIn("session", new UserProfile("admin", "admin", "sashaudalv@gmail.com", "avatar"));
-
-        when(request.getSession()).thenReturn(SessionHelper.getNewSession("session1"));
-        when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
-
-        AdministrationServlet administrationServlet = new AdministrationServlet(accountService);
-        administrationServlet.doGet(request, response);
-
-        assertTrue(stringWriter.toString().contains("You are not authorised."));
     }
 
     @Test
-    public void testWithoutParameter() throws Exception {
-        StringWriter stringWriter = new StringWriter();
+    public void testNotLoggedIn() throws ServletException, IOException {
+        try (StringWriter stringWriter = new StringWriter()) {
+            when(session.getId()).thenReturn("session1");
+            when(request.getSession()).thenReturn(session);
+            when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
 
-        accountService.signIn("session", new UserProfile("admin", "admin", "sashaudalv@gmail.com", "avatar"));
+            AdministrationServlet administrationServlet = new AdministrationServlet(accountService);
+            administrationServlet.doGet(request, response);
 
-        when(request.getSession()).thenReturn(SessionHelper.getNewSession("session"));
-        when(request.getParameter("shutdown")).thenReturn(null);
-        when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
-
-        AdministrationServlet administrationServlet = new AdministrationServlet(accountService);
-        administrationServlet.doGet(request, response);
-
-        assertTrue(stringWriter.toString().contains("Users: 0"));
-        assertTrue(stringWriter.toString().contains("Logged users: 1"));
+            assertTrue(stringWriter.toString().contains("You are not authorised."));
+        }
     }
 
     @Test
-    public void testNotAdministration() throws Exception {
-        StringWriter stringWriter = new StringWriter();
+    public void testAdministratorPage() throws ServletException, IOException {
+        try (StringWriter stringWriter = new StringWriter()) {
+            when(session.getId()).thenReturn("session");
+            when(request.getSession()).thenReturn(session);
+            when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
 
-        accountService.signIn("session", new UserProfile("admin", "admin", "admin", "avatar"));
+            AdministrationServlet administrationServlet = new AdministrationServlet(accountService);
+            administrationServlet.doGet(request, response);
 
-        when(request.getSession()).thenReturn(SessionHelper.getNewSession("session"));
-        when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
-
-        AdministrationServlet administrationServlet = new AdministrationServlet(accountService);
-        administrationServlet.doGet(request, response);
-
-        assertTrue(stringWriter.toString().contains("You don't have rights of administrator."));
-
+            assertTrue(stringWriter.toString().contains("Users: 1"));
+            assertTrue(stringWriter.toString().contains("Logged users: 1"));
+        }
     }
 
     @Test
-    public void testAdministration() throws Exception {
-        StringWriter stringWriter = new StringWriter();
+    public void testNotAdministrator() throws ServletException, IOException {
+        try (StringWriter stringWriter = new StringWriter()) {
+            accountService.signIn("session1", new UserProfile("notadmin", "notadmin", "notadmin", "avatar"));
 
-        accountService.signIn("session", new UserProfile("admin", "admin", "sashaudalv@gmail.com", "avatar"));
+            when(session.getId()).thenReturn("session1");
+            when(request.getSession()).thenReturn(session);
+            when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
 
-        when(request.getSession()).thenReturn(SessionHelper.getNewSession("session"));
-        when(request.getParameter("shutdown")).thenReturn("5000");
-        when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
+            AdministrationServlet administrationServlet = new AdministrationServlet(accountService);
+            administrationServlet.doGet(request, response);
 
-        AdministrationServlet administrationServlet = new AdministrationServlet(accountService);
-        administrationServlet.doGet(request, response);
+            assertTrue(stringWriter.toString().contains("You don't have rights of administrator."));
+        }
+    }
+
+    @Test
+    public void testServerShutdown() throws ServletException, IOException {
+        try (StringWriter stringWriter = new StringWriter()) {
+            exit.expectSystemExitWithStatus(0);
+            when(session.getId()).thenReturn("session");
+            when(request.getSession()).thenReturn(session);
+            when(request.getParameter("shutdown")).thenReturn("0");
+            when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
+
+            AdministrationServlet administrationServlet = new AdministrationServlet(accountService);
+            administrationServlet.doGet(request, response);
+        }
     }
 }
