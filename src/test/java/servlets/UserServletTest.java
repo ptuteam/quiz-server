5d45377dafe489dd248f27441ffc0f4d17e1aeb1
alex <sashaudalv@gmail.com>
2015-11-02 01:52:03 +0300

    UserServletTest fix

--- src/test/java/servlets/UserServletTest.java
+++ src/test/java/servlets/UserServletTest.java
@@ -1,63 +1,74 @@
 package servlets;
 
 import model.UserProfile;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 import utils.AccountService;
 import utils.AccountServiceImpl;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static utils.SessionHelper.getNewSession;
 
 /**
  * Created by Dima on 26.10.2015.
  */
 public class UserServletTest {
-    private final static HttpServletRequest request = mock(HttpServletRequest.class);
-    private final static HttpServletResponse response = mock(HttpServletResponse.class);
-    private final static AccountService accountService = new AccountServiceImpl();
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+    private AccountService accountService;
+    private HttpSession session;
 
-    @BeforeClass
-    public static void setUp() throws Exception {
+    @Before
+    public void setUp() {
+        request = mock(HttpServletRequest.class);
+        response = mock(HttpServletResponse.class);
+        accountService = new AccountServiceImpl();
+        session = mock(HttpSession.class);
         UserProfile user = new UserProfile("first", "last", "email", "avatar");
         user.setScore(1);
         accountService.signIn("session", user);
     }
 
     @Test
-    public void testUser() throws Exception {
-        StringWriter stringWriter = new StringWriter();
+    public void testUser() throws ServletException, IOException {
+        try (StringWriter stringWriter = new StringWriter()) {
+            when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
+            when(session.getId()).thenReturn("session");
+            when(request.getSession()).thenReturn(session);
 
-        when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
-        when(request.getSession()).thenReturn(getNewSession("session"));
+            UserServlet userServlet = new UserServlet(accountService);
+            userServlet.doGet(request, response);
 
-        new UserServlet(accountService).doGet(request, response);
-
-        assertTrue(stringWriter.toString().contains(
-                "\"first_name\":\"first\"," +
-                "\"last_name\":\"last\"," +
-                "\"email\":\"email\"," +
-                "\"avatar\":\"avatar\"," +
-                "\"score\":1"));
+            assertTrue(stringWriter.toString().contains(
+                            "\"first_name\":\"first\"," +
+                                    "\"last_name\":\"last\"," +
+                                    "\"email\":\"email\"," +
+                                    "\"avatar\":\"avatar\"," +
+                                    "\"score\":1")
+            );
+        }
     }
 
     @Test
-    public void testUserNotLogged() throws Exception {
-        StringWriter stringWriter = new StringWriter();
+    public void testUserNotLogged() throws ServletException, IOException {
+        try (StringWriter stringWriter = new StringWriter()) {
+            when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
+            when(session.getId()).thenReturn("session1");
+            when(request.getSession()).thenReturn(session);
 
-        when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
-        when(request.getSession()).thenReturn(getNewSession("session1"));
+            UserServlet userServlet = new UserServlet(accountService);
+            userServlet.doGet(request, response);
 
-        new UserServlet(accountService).doGet(request, response);
-
-        assertTrue(stringWriter.toString().contains("You have not been logged"));
+            assertTrue(stringWriter.toString().contains("You have not been logged"));
+        }
     }
 }
