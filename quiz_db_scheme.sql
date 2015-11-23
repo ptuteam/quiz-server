11bbe77b4a67eedd025e207b3932539a7f227eb0
dima <dimont95@mail.ru>
2015-11-23 17:45:14 +0300

    quiz_db_scheme.sql updated DatabaseConnectionTest added

--- quiz_db_scheme.sql
+++ quiz_db_scheme.sql
@@ -19,12 +19,12 @@
 -- Table structure for table `answer`
 --
 
-DROP DATABASE IF EXISTS quiz_db;
+DROP DATABASE IF EXISTS test_quiz_db;
 
-CREATE DATABASE quiz_db;
-GRANT ALL PRIVILEGES ON quiz_db.* to quiz_user@localhost identified by 'secret';
+CREATE DATABASE test_quiz_db;
+GRANT ALL PRIVILEGES ON test_quiz_db.* to test_quiz_user@localhost identified by 'secret';
 
-USE quiz_db;
+USE test_quiz_db;
 
 DROP TABLE IF EXISTS `answers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -44,10 +44,10 @@ CREATE TABLE `answers` (
 -- Dumping data for table `answers`
 --
 
-LOCK TABLES `answer` WRITE;
-/*!40000 ALTER TABLE `answer` DISABLE KEYS */;
-INSERT INTO `answer` VALUES (1,'answer 1, question 1',1,1), (1,'answer 2, question 1',0,2), (1,'answer 3, question 1',0,3), (1,'answer 4, question 1',0,4), (2,'answer, question 2',1,5);
-/*!40000 ALTER TABLE `answer` ENABLE KEYS */;
+LOCK TABLES `answers` WRITE;
+/*!40000 ALTER TABLE `answers` DISABLE KEYS */;
+INSERT INTO `answers` VALUES (1,'answer 1, question 1',1,1), (1,'answer 2, question 1',0,2), (1,'answer 3, question 1',0,3), (1,'answer 4, question 1',0,4), (2,'answer, question 2',1,5);
+/*!40000 ALTER TABLE `answers` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -70,10 +70,10 @@ CREATE TABLE `questions` (
 -- Dumping data for table `question`
 --
 
-LOCK TABLES `question` WRITE;
-/*!40000 ALTER TABLE `question` DISABLE KEYS */;
-INSERT INTO `question` VALUES (1,'question1',1), (2,'question2',2);
-/*!40000 ALTER TABLE `question` ENABLE KEYS */;
+LOCK TABLES `questions` WRITE;
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+INSERT INTO `questions` VALUES (1,'question1',1), (2,'question2',2);
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -86,3 +86,65 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2015-11-14  0:03:20
+
+-- MySQL dump 10.13  Distrib 5.5.46, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: users_db
+-- ------------------------------------------------------
+-- Server version	5.5.46-0ubuntu0.14.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+CREATE DATABASE test_quiz_users_db;
+GRANT ALL PRIVILEGES ON test_quiz_users_db.* to test_quiz_user@localhost;
+USE test_quiz_users_db;
+
+--
+-- Table structure for table `users`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(63) NOT NULL,
+  `last_name` varchar(63) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `avatarUrl` varchar(255) NOT NULL,
+  `score` int(11) NOT NULL DEFAULT '0',
+  `is_guest` int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE users DROP COLUMN is_guest;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-11-19 22:26:12
---
+++ src/test/java/database/connection/DatabaseConnectionTest.java
@@ -0,0 +1,85 @@
+package database.connection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import utils.ConfigGeneral;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by dima on 23.11.15.
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({DatabaseConnection.class, ConfigGeneral.class})
+public class DatabaseConnectionTest {
+
+    @Before
+    public void setUp() throws Exception {
+        PowerMockito.mockStatic(ConfigGeneral.class);
+        when(ConfigGeneral.getDbType()).thenReturn("jdbc:mysql://");
+        when(ConfigGeneral.getDbHostName()).thenReturn("localhost:");
+        when(ConfigGeneral.getDbPort()).thenReturn("3306/");
+        when(ConfigGeneral.getDbNameQuiz()).thenReturn("test_quiz_db?");
+        when(ConfigGeneral.getDbLogin()).thenReturn("user=test_quiz_user&");
+        when(ConfigGeneral.getDbPassword()).thenReturn("password=secret");
+
+
+    }
+
+    @Test
+    public void testGetQuizConnection() throws SQLException {
+
+        Connection connection = DatabaseConnection.getQuizConnection();
+
+        if (connection == null) {
+            System.out.println("Please install test database by executing quiz_db_scheme.sql" +
+                    " from root in your mysql server.");
+        } else {
+            assertEquals("test_quiz_user@localhost", connection.getMetaData().getUserName());
+        }
+    }
+
+    @Test
+    public void testGetUsersConnection() throws SQLException {
+        PowerMockito.mockStatic(ConfigGeneral.class);
+        when(ConfigGeneral.getDbType()).thenReturn("jdbc:mysql://");
+        when(ConfigGeneral.getDbHostName()).thenReturn("localhost:");
+        when(ConfigGeneral.getDbPort()).thenReturn("3306/");
+        when(ConfigGeneral.getDbNameUsers()).thenReturn("test_quiz_users_db?");
+        when(ConfigGeneral.getDbLogin()).thenReturn("user=test_quiz_user&");
+        when(ConfigGeneral.getDbPassword()).thenReturn("password=secret");
+
+        Connection connection = DatabaseConnection.getUsersConnection();
+
+        if (connection == null) {
+            System.out.println("Please install test database by executing quiz_db_scheme.sql" +
+                    " from root in your mysql server.");
+        } else {
+            assertEquals("test_quiz_user@localhost", connection.getMetaData().getUserName());
+        }
+    }
+
+    @Test
+    public void testGetConnectionWrongDB() throws SQLException {
+        PowerMockito.mockStatic(ConfigGeneral.class);
+        when(ConfigGeneral.getDbType()).thenReturn("jdbc:mysql://");
+        when(ConfigGeneral.getDbHostName()).thenReturn("localhost:");
+        when(ConfigGeneral.getDbPort()).thenReturn("3306/");
+        when(ConfigGeneral.getDbNameUsers()).thenReturn("test_quiz_users_db1?");
+        when(ConfigGeneral.getDbLogin()).thenReturn("user=test_quiz_user&");
+        when(ConfigGeneral.getDbPassword()).thenReturn("password=secret");
+
+        Connection connection = DatabaseConnection.getUsersConnection();
+
+        assertTrue(connection == null);
+    }
+}
